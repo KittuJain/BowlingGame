97bcbd4027bb9c9bb1d09e6c8d79c2cc52517df4
KratiJain <cskratijain@gmail.com>
2017-05-26 07:56:07 +0530

    Cleaning the code to make it more understandable

--- src/main/com/thoughtworks/BowlingGame.java
+++ src/main/com/thoughtworks/BowlingGame.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 class BowlingGame {
-    private List<Integer> rolls = new ArrayList();
+    private List<Integer> rolls = new ArrayList<>();
 
     public void roll( int pins ) {
         rolls.add( pins );
@@ -13,27 +13,22 @@ class BowlingGame {
     public int getScore() {
         int score = 0;
         int rollIndex = 0;
-        int frame = 0;
 
-        while ( frame <= 9 ) {
+        for ( int counter = 0; counter < 9; counter++ ) {
             if ( isStrike( rollIndex ) ) {
-                score += 10 + rolls.get( rollIndex + 1 ) + rolls.get( rollIndex + 2 );
+                score += 10 + bonusScoreByStrike( rollIndex );
                 rollIndex += 1;
-                frame++;
             }
             if ( isSpare( rollIndex ) ) {
-                score += 10 + rolls.get( rollIndex + 2 );
+                score += 10 + bonusScoreBySpare( rollIndex );
                 rollIndex += 2;
-                frame++;
             } else {
-                score += rolls.get( rollIndex ) + rolls.get( rollIndex + 1 );
+                score += getScoreOfCurrentFrame( rollIndex );
                 rollIndex += 2;
-                frame++;
             }
         }
         return score;
     }
-
     private boolean isSpare( int rollIndex ) {
         return rolls.get( rollIndex ) + rolls.get( rollIndex + 1 ) == 10;
     }
@@ -41,5 +36,18 @@ class BowlingGame {
     private boolean isStrike( int rollIndex ) {
         return rolls.get( rollIndex ) == 10;
     }
+
+    private int bonusScoreByStrike( int rollIndex ) {
+        return rolls.get( rollIndex + 1 ) + rolls.get( rollIndex + 2 );
+    }
+
+    private int bonusScoreBySpare( int rollIndex ) {
+        return rolls.get( rollIndex + 2 );
+    }
+
+    private int getScoreOfCurrentFrame( int rollIndex ) {
+        return rolls.get( rollIndex ) + rolls.get( rollIndex + 1 );
+    }
+
 
 }
