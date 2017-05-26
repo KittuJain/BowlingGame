702e4132df7e7afb0b7dcab6750288406714fd5e
KratiJain <cskratijain@gmail.com>
2017-05-26 07:34:36 +0530

    Handling strike and spare in the game

--- src/main/com/thoughtworks/BowlingGame.java
+++ src/main/com/thoughtworks/BowlingGame.java
@@ -1,17 +1,45 @@
 package com.thoughtworks;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class BowlingGame {
-    private int score = 0;
+    private List<Integer> rolls = new ArrayList();
+
+    public void roll( int pins ) {
+        rolls.add( pins );
+    }
 
     public int getScore() {
+        int score = 0;
+        int rollIndex = 0;
+        int frame = 0;
+
+        while ( frame <= 9 ) {
+            if ( isStrike( rollIndex ) ) {
+                score += 10 + rolls.get( rollIndex + 1 ) + rolls.get( rollIndex + 2 );
+                rollIndex += 1;
+                frame++;
+            }
+            if ( isSpare( rollIndex ) ) {
+                score += 10 + rolls.get( rollIndex + 2 );
+                rollIndex += 2;
+                frame++;
+            } else {
+                score += rolls.get( rollIndex ) + rolls.get( rollIndex + 1 );
+                rollIndex += 2;
+                frame++;
+            }
+        }
         return score;
     }
 
-    public void roll( int pins ) {
-        setScore( pins );
+    private boolean isSpare( int rollIndex ) {
+        return rolls.get( rollIndex ) + rolls.get( rollIndex + 1 ) == 10;
     }
 
-    public void setScore( int score ) {
-        this.score += score;
+    private boolean isStrike( int rollIndex ) {
+        return rolls.get( rollIndex ) == 10;
     }
+
 }
--- src/test/com/thoughtworks/BowlingGameTest.java
+++ src/test/com/thoughtworks/BowlingGameTest.java
@@ -1,29 +1,33 @@
 package com.thoughtworks;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class BowlingGameTest {
 
+    private BowlingGame bowlingGame;
+
+    @Before
+    public void setUp() throws Exception {
+        bowlingGame = new BowlingGame();
+    }
+
     @Test
     public void failsToKnockEvenSinglePin() {
-        final BowlingGame bowlingGame = new BowlingGame();
-        bowlingGame.roll( 0 );
-        bowlingGame.roll( 0 );
+        rollEmpty( 20 );
         Assert.assertEquals( 0, bowlingGame.getScore() );
     }
 
     @Test
-    public void knockOfSinglePinResultsInScore1() {
-        final BowlingGame bowlingGame = new BowlingGame();
+    public void getsScoreWhenSinglePinIsKnocked() {
         bowlingGame.roll( 1 );
-        bowlingGame.roll( 0 );
+        rollEmpty( 19 );
         Assert.assertEquals( 1, bowlingGame.getScore() );
     }
 
     @Test
     public void getsScoreOfNumberOfBallsKnocked() {
-        final BowlingGame bowlingGame = new BowlingGame();
         bowlingGame.roll( 5 );
         bowlingGame.roll( 2 );
         bowlingGame.roll( 6 );
@@ -34,7 +38,41 @@ public class BowlingGameTest {
         bowlingGame.roll( 7 );
         bowlingGame.roll( 6 );
         bowlingGame.roll( 8 );
+        rollEmpty( 10 );
         Assert.assertEquals( 54, bowlingGame.getScore() );
     }
+
+    @Test
+    public void getsScoreWhenNoBonusGivenOnSpare() {
+        bowlingGame.roll( 5 );
+        bowlingGame.roll( 5 );
+        rollEmpty( 18 );
+        Assert.assertEquals( 10, bowlingGame.getScore() );
+    }
+
+    @Test
+    public void getsScoreWhenBonusGivenOnSpare() {
+        bowlingGame.roll( 7 );
+        bowlingGame.roll( 3 );
+        bowlingGame.roll( 3 );
+        rollEmpty( 17 );
+        Assert.assertEquals( 16, bowlingGame.getScore() );
+    }
+
+    @Test
+    public void getsScoreOnStrike() {
+        bowlingGame.roll( 10 );
+        bowlingGame.roll( 3 );
+        bowlingGame.roll( 4 );
+        rollEmpty( 17 );
+        Assert.assertEquals( 24, bowlingGame.getScore() );
+    }
+
+    private void rollEmpty( int times ) {
+        for ( int i = 0; i < times; i++ ) {
+            bowlingGame.roll( 0 );
+        }
+    }
 }
+
 
